6b99e66892ff64162ef6716757174cb6d6cbffcb
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-04-09 01:19:48 +0530

    created the data

---
+++ day3_3assignments/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import data from "./db.json";
+function App() {
+  console.log(data)
+  return (
+    <div className="App">
+     <h1>coupons</h1>
+    </div>
+  );
+}
+
+export default App;
