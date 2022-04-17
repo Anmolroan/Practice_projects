4ab1f019b5016e1de897a8228fc102c5e8b8656c
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-04-18 00:58:47 +0530

    stuck in timer

--- stopwatch/src/components/Timer/Timer.js
+++ stopwatch/src/components/Timer/Timer.js
@@ -5,14 +5,26 @@ function Timer() {
     // const [minute,setMinutes]= useState(0);
     // const [seconds,setSeconds]= useState(0);
     // const [hour,setHour]=useState(0);
-    const [value,setValue]= useState("")
-    const [value2,setValue2]= useState("")
-    const [value3,setValue3]= useState("")
+    const [value,setValue]= useState(0)
+    const [value2,setValue2]= useState(0)
+    const [value3,setValue3]= useState(0)
+    const [stop,setStop]= useState(false)
     const [set,reSet]=useState(true);
     const inputSec =useRef(null);
     const inputMin =useRef(null);
     const inputHour =useRef(null);
+    const [flag,setFlag]=useState(false);
+useEffect(()=>{
+if(flag){
+   const interval=setInterval(()=>{
+
  
+  },1000)
+  return ()=>{
+    clearInterval(interval)
+  }
+}
+},[flag])
   return (
    
     <div>
@@ -21,22 +33,36 @@ function Timer() {
       set ?  <div>
      
       
-<span>{inputHour.current!==null?inputHour.current.value :"00"}</span>
-<span>{inputMin.current!==null?inputMin.current.value :"00"}</span>
-<span>{inputSec.current!==null?inputSec.current.value :"00"}</span>
+<span>{value}</span>
+<span>{value2}</span>
+<span>{value3}</span>
 
     </div>
     :  <div>
-      <input ref={inputHour}  value={value} onChange={(e)=>{setValue(e.target.value)}} name="hour"/>
-      <input ref={inputMin}  value={value2} onChange={(e)=>{setValue2(e.target.value)}}  name="min"/>
-      <input ref={inputSec} value={value3} onChange={(e)=>{setValue3(e.target.value)}} />
+      <input ref={inputHour}   onChange={(e)=>{setValue(e.target.value)}} type="Number"/>
+      <input ref={inputMin}   onChange={(e)=>{setValue2(e.target.value)}}  type="Number"/>
+      <input ref={inputSec}  onChange={(e)=>{setValue3(e.target.value)}} type="Number"/>
     </div>
     }
     
  
   
-    <button  onClick ={()=>reSet(true)}> start </button>
-<button  onClick ={()=>reSet(false)}>Reset</button>
+ 
+   {  !stop ?
+     <button  onClick ={()=>{
+      reSet(true);
+      setFlag(true);
+      setStop(true)
+      }}> Start </button>
+      :<button onClick={()=>{
+        setStop(false);
+        setFlag(false);
+        }}>Stop</button>}
+    
+<button  onClick ={()=>{
+  reSet(false)
+ 
+  }}>Reset</button>
    
    
     
