bed419d1637e71f99367897d6a581f95cc944548
Anmolroan <anmol.kumar@masaischool.com>
2022-05-19 14:47:32 +0530

    will solve the error later

--- tsreact/src/components/Todo.tsx
+++ tsreact/src/components/Todo.tsx
@@ -2,17 +2,24 @@ import React from 'react'
 import {v4 as uuid} from "uuid";
 // npm install --save-dev @types/packagename
 import axios from "axios";
+import AxiosResponse from "axios"
 type TodoType ={id:string;
   title:string;
   status:boolean}
 function Todo() {
   const [text,setText]=React.useState("");
   const [todo,setTodo]=React.useState <TodoType[]>([]);
+  const getData =()=>{
+    axios.get("http://localhost:8080/todo")
+    .then((response:AxiosResponse<TodoType[]>)=>{
+      const {data}=response;
+    })
+  }
   return (
     <div>
         <input value={text} onChange={(e)=>setText(e.target.value)}/>
         <button onClick={()=>{
-          const payload ={id:uuid(),title:text,status:false}
+          const payload ={title:text,status:false}
           // setTodo([...todo,payload]);
           axios.post("http://localhost:8080/todo",payload)
         }}>ADD</button>
