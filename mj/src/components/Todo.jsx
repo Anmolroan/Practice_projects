d369b51d1d28c1b4cc59747715c01b09c0605249
Anmolroan <anmol.kumar@masaischool.com>
2022-05-19 22:39:23 +0530

    mj

--- mj/src/components/Todo.jsx
+++ mj/src/components/Todo.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import{useState} from 'react'
+import TodoList from './TodoList';
+import { nanoid } from 'nanoid'
 function Todo() {
     const [text,setText]=useState("");
     const [data,setData]=useState([]);
     const handleAdd =()=>{
-        setData([...data,text]);
+      const payload ={
+        id:nanoid(),
+        title:text,
+      }
+        setData([...data,payload]);
+        setText("");
         console.log(data)
     }
+   
   return (
     <div>
         <input value={text} onChange={(e)=>{setText(e.target.value)}}/>
         <button onClick={handleAdd}>add</button>
-    
+        <TodoList data={data} />
+    {/* {data.map((el)=>{
+     return <div>{el}</div>
+    })} */}
     </div>
   )
 }
