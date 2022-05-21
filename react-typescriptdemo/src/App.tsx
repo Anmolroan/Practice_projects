5f4b1f07cee2c1f5cd96d0f22d873dd2b695a035
Anmolroan <anmol.kumar@masaischool.com>
2022-05-21 21:01:09 +0530

    5 vid

--- react-typescriptdemo/src/App.tsx
+++ react-typescriptdemo/src/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 import Greet from './components/Greet';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
+import Status from './components/Status';
+import Heading from './components/Heading'
+import Oscar from './components/Oscar';
 function App() {
   const  personName ={
     first:"Bruce",
@@ -21,9 +24,11 @@ function App() {
   }]
   return (
     <div className="App">
-    <Greet name="anmol" msg={10} isLoggedIn={true} />
+    {/* <Greet name="anmol" msg={10} isLoggedIn={true} />
     <Person name={personName}/>
     <PersonList names={nameList}/>
+    <Status status='error'/> */}
+    <Oscar><Heading> oscar goes to Anmol</Heading></Oscar>
     </div>
   );
 }
