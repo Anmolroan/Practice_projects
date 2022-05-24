4a43d24ccea54f6e87e65ccabef65123eb595d06
Anmolroan <anmol.kumar@masaischool.com>
2022-05-24 16:33:08 +0530

    permofance optimaztion

--- custom-hooks/src/App.tsx
+++ custom-hooks/src/App.tsx
@@ -12,21 +12,21 @@ type UserType ={
 function App() {
   const [text,setText]=useState("");
   const [data,setData] = useState<UserType[]>([])
-const {data,err,loading} =useFetch(URL,{
+const {err,loading} =useFetch(URL,{
   per_page:5,
   q:text
 })
 return ( <div className="App">
     <input value={text} onChange={(e)=>setText(e.target.value)}/>
     <button onClick={()=>{
-      axios.get(URL,{
-        params:{
-          per_page:5,
-          q:text
-        }
-      }).then(({data})=>{
-        setData(data.items)
-      })
+      // axios.get(URL,{
+      //   params:{
+      //     per_page:5,
+      //     q:text
+      //   }
+      // }).then(({data})=>{
+      //   setData(data.items)
+      // })
     }}>Search</button>
          {/* <h1>Hello</h1> */}
          {data.map((user)=>{
