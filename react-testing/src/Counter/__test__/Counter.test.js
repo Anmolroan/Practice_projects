ee24956575976ad341e1365037d8aa06eb5093a7
Anmolroan <anmol.kumar@masaischool.com>
2022-05-25 19:54:28 +0530

    testing

--- react-testing/src/Counter/__test__/Counter.test.js
+++
@@ -1,7 +0,0 @@
-
-import Counter from '../Counter'
-import {render} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-test("header render with correct test",() => {
-  
-})
