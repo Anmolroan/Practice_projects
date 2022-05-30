c4041f9dc5f3a76bde7088ac9878bd873800dcb6
Anmolroan <anmol.kumar@masaischool.com>
2022-05-30 20:08:37 +0530

    hook

---
+++ hookscode/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import WithObject from './components/WithObject'
+import WithArr from './components/usestate/WithArr'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <h4>Hooks</h4>
+   <WithArr/>
+    </div>
+  )
+}
+
+export default App
