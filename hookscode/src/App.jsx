df219eed5a503c5cbb2e5d32f840af79502ee292
Anmolroan <anmol.kumar@masaischool.com>
2022-05-31 17:38:01 +0530

    react useeffect

--- hookscode/src/App.jsx
+++ hookscode/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import WithObject from './components/WithObject'
-import WithArr from './components/usestate/WithArr'
+import HookMouse from './components/useEffect/HookMouse'
+// import HookCounterone from './components/useEffect/HookCounterone'
+// import WithObject from './components/WithObject'
+// import WithArr from './components/usestate/WithArr'
 
 function App() {
  
@@ -10,7 +12,9 @@ function App() {
   return (
     <div className="App">
     <h4>Hooks</h4>
-   <WithArr/>
+   {/* <WithArr/> */}
+   {/* <HookCounterone/> */}
+   <HookMouse/>
     </div>
   )
 }
