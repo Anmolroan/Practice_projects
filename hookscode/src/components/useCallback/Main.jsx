099ae1b28f929426cf4c6f2b027e5d6754a439b7
Anmolroan <anmol.kumar@masaischool.com>
2022-07-04 02:16:23 +0530

    callback

--- hookscode/src/components/useCallback/Main.jsx
+++ hookscode/src/components/useCallback/Main.jsx
@@ -5,9 +5,14 @@ import Two from './Two';
 
 function Main() {
     const [count,setCount]= useState(0);
+    const [counter,setCounter]= useState(5);
+    const [counter3,setCounter3]= useState(10);
 console.log("main")
 const handleUpdate =() => {
     setCount(count+1);
+}
+const handleUpdate2=()=>{
+
 }
   return (
     <div>
